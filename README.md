f282bb27f3d93acb88ab8bda6a0c78af253469e9
Enes Gönültaş <nsgonultas@gmail.com>
2017-03-19 11:27:04 +0300

    İlk commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sezyum
+
+Sezyum Redmine projesi için geliştirilmiş bir eklentidir. Sezyumu kullanarak projedeki her üyenin hangi işler üzerinde ne kadar süre çalıştığını görebilirsiniz.
---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :sezyum do
+  name 'Sezyum'
+  author 'Enes Gönültaş'
+  description 'Redmine için Sezyum eklentisi'
+  version '0.0.1'
+  # url 'http://example.com/path/to/plugin'
+  # author_url 'http://example.com/about'
+end
